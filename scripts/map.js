e71eb6fb35b7a32a98b61c1853b0f86673edc2b2
Dennis Kimani <denoh6804@gmail.com>
2019-11-22 14:10:50 +0300

    Minor changes

--- scripts/map.js
+++ scripts/map.js
@@ -1,6 +1,44 @@
+// defining variables
+const keyWeather = "3273a2625ba19b34df4e656691e48b57";
+const mapApiKey =
+  "pk.eyJ1IjoiZGVub2hraW0iLCJhIjoiY2syeDFtZzlvMDdjbjNib21tODh6dm1rZCJ9.fvIAC_Jb9j0iWGOcsaEDPw";
+const city = "Nairobi";
+
 // select DOM elements
+const temp = document.querySelector(".temp");
+const icon = document.querySelector(".weather-icon");
+const tempDesc = document.querySelector(".temp-desc");
 // get coordinates
+function getCoordinates() {
+  fetch(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=${mapApiKey}`
+  )
+    .then(res => {
+      const data = res.json();
+      return data;
+    })
+    .then(data => {
+      let long = data.features[0].geometry.coordinates[0];
+      let lat = data.features[0].geometry.coordinates[1];
+    });
+}
+
+getCoordinates();
+
 // get weather
+function getWeather(lat, long) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${keyWeather}`
+  )
+    .then(res => {
+      const data = res.json();
+      return data;
+    })
+    .then(data => {
+      console.log(data.coord.lat);
+      // console.log(data.coord.long);
+    });
+}
 // display the info to the user
 // draw a marker on the map with the place of interest
 
